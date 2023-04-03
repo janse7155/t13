bac491cda055326fd90e46fecae75993e052c87f
JRSeabaugh1s <JRSeabaugh1s@capetigers.com>
2023-04-03 09:18:12 -0500

    add price calculation

--- co_order.js
+++ co_order.js
@@ -32,5 +32,22 @@ window.addEventListener("load", function() {
       var orderForm = document.forms.orderForm;
       orderForm.elements.orderDate.value = new Date(),toDateString();
       orderForm.elements.model.focus();
+
+//Retrieve the cost of user's protection plan
+var pCost = document.querySelector('input[name="protection"]: checked').value*quantity;
+orderForm.elements.protectionCost.value = pCost;
+
+//Calculate the order subtotal
+orderForm.elements.subtotal.value = initialCost + pCost;
+
+//Calculate the sales tax
+var salesTax = 0.05*(initialCost + pCost);
+orderForm.elements.salesTax.value = salesTax;
+
+//Calculate the total cost of the order
+var totalCost = initialCost + pCost + salesTax;
+orderForm.elements.totalCost.value = totalCost;
    }
 );
+
+
